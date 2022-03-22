d4d18d9f88a9b91685b007b6eed116b5dde8396d
aninditaroy <anindita.roy.mu@gmail.com>
2022-03-22 19:52:44 +0600

    first commit

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import './Shop.css';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products.map(product => <Product key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                    />)
+                }
+            </div>
+            <div className="cart-container">
+                <h4>Order summary</h4>
+                <p>Selected items: {cart.length}</p>
+                <p>Total price: ${ }</p>
+                <p>Total Shipping Charge: { }</p>
+                <p>Tax: { }</p>
+                <h4>Grand Total: { }</h4>
+                <button className='clear-cart'>
+                    <p className='btn-text-1'>Clear Cart</p>
+                    <FontAwesomeIcon icon={faArrowRight}/>
+                </button>
+                <button className='review-order'>
+                    <p className='btn-text-2'>Review Order</p>
+                    <FontAwesomeIcon icon={faTrashCan} />
+                </button>
+
+            </div>
+        </div>
+    );
+};
+export default Shop;
